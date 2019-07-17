77ade10152e676edb3428b9d131c867aaac2f73e
zhujunxi <531578783@qq.com>
2019-07-17 14:29:32 +0800

    [feat] 添加gzip与开启js压缩

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -2,15 +2,23 @@ const merge = require("webpack-merge");
 const path = require("path");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const baseWebpackConfig = require("./webpack.base.conf");
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
 module.exports = merge(baseWebpackConfig, {
   mode: "production",
   plugins: [
-    new CleanWebpackPlugin(["dist", "demo"], {
+    new CleanWebpackPlugin(["dist"], {
       root: path.resolve(__dirname, "../"),
+    }),
+    // Gzip配置
+    new CompressionWebpackPlugin({
+      test: new RegExp('\\.(' + ['js', 'css'].join('|') + ')$'),
+      threshold: 8192,
+      minRatio: 0.8
     })
+
   ],
   optimization: {
-    minimize: false
+    minimize: true
   }
 });

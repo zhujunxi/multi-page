e9aa8bd12eb065851a64523bc07b60cb95ad8dee
zhujunxi <531578783@qq.com>
2019-07-17 10:48:41 +0800

    [feat] 优化html

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
-// import "./index.less";
-import img from "./assets/image/up_logo.png";
+
 new Vue({
-  data: {
-    msg: "hello",
-    src: img
-  },
   render: h => h(App)
 }).$mount("#app");
